4d869e23e3d18247ebec03c5682034350bb73c0a
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-01-17 12:05:01 +0530

    Time: 25 ms (32.58%), Space: 19.2 MB (39.81%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, arr: List[int]) -> int:
+        n=len(arr)
+        lmax=0
+        rmax=0
+        l=0
+        r=n-1
+        ans=0
+        while l<=r:
+            lmax=max(arr[l],lmax)
+            rmax=max(arr[r],rmax)
+            if arr[l]<=arr[r]:
+                ans+=min(lmax,rmax)-arr[l]
+                
+                l+=1
+            else:
+                ans+=min(lmax,rmax)-arr[r]
+                
+                r-=1
+        return ans
