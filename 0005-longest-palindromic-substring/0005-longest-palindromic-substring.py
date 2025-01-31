10fa3cf859c7ca4ab1752339d4a9d1b15f058c39
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-01-31 15:44:54 +0530

    Time: 4702 ms (14.05%), Space: 113.5 MB (5.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,24 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        dp=[[False]*n for i in range(n)]
-        start, max_len = 0, 0
-
-        # Fill the DP table
-        for length in range(1, n + 1):  # Length of the substring
-            for i in range(n - length + 1):
-                j = i + length - 1  # Ending index of the substring
-                
-                if length == 1:
-                    dp[i][j] = True  # Single character
-                elif length == 2:
-                    dp[i][j] = (s[i] == s[j])  # Two characters
-                else:
-                    dp[i][j] = (s[i] == s[j]) and dp[i + 1][j - 1]  # More than two characters
-                
-                # Update the longest palindrome if this substring is valid
-                if dp[i][j] and length > max_len:
-                    start, max_len = i, length
+        #because we are going through all of the substrings
         
-        # Return the longest palindrome substring
-        return s[start:start + max_len]
+
+        dp=[[-1]*len(s) for i in range(len(s))]
+        def solve(i,j):
+            if dp[i][j]==1 or dp[i][j]==0:
+                return dp[i][j]
+            if i>=j:
+                dp[i][j]=True
+                return dp[i][j]
+            if s[i]==s[j]:
+                dp[i][j]=solve(i+1,j-1)
+                return dp[i][j]
+            else:
+                dp[i][j]=False
+                return dp[i][j]
+        startpoint=-1
+        leng=0
+        for i in range(0,len(s)):
+            for j in range(i,len(s)):
+                if solve(i,j):
+                    
+                    if leng<j-i+1:
+                        leng=j-i+1
+                        startpoint=i
+        return s[startpoint:leng+startpoint]
