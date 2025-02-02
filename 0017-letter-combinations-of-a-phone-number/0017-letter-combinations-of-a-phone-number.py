10a1b1fcfbf921a013c3575e671b9bf1a3f308fe
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-02 20:09:29 +0530

    Time: 0 ms (100%), Space: 17.8 MB (55.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,32 +1,16 @@
 class Solution:
-    def letterCombinations(self, d: str) -> List[str]:
+    def letterCombinations(self, digits: str) -> List[str]:
         dic={
-            "2":"abc",
-            "3":"def",
-            "4":"ghi",
-            "5":"jkl",
-            "6":"mno",
-            "7":"pqrs",
-            "8":"tuv",
-            "9":"wxyz"
+            2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"
         }
-        ans=[]
-        s=""
-        if d=="":
+        n=len(digits)
+        def recur(i,s):
+            if i==n:
+                return [s]
+            l=[]
+            for index in dic[int(digits[i])]:
+                l+=recur(i+1,s+index)
+            return l
+        if digits=="":
             return []
-
-        def kartik(d,ind,l):
-            if ind==n:
-                ans.append(l)
-                return 
-            for i in dic[d[ind]]:
-                pick=kartik(d,ind+1,l+i)
-            return 
-        n=len(d)
-        ind=0
-        l=""
-        x=kartik(d,ind,l)
-        ans.sort()
-        return ans
-
-        
+        return recur(0,"")
