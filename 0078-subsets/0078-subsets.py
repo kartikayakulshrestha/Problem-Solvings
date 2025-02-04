db2e855f86f55828b78e57ec7a071ac1fcc6f46a
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-04 13:40:17 +0530

    Time: 0 ms (100%), Space: 17.8 MB (74.6%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,9 +1,11 @@
-from itertools import combinations
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans=[]
-        for i in range(len(nums)+1):
-            l=combinations(nums,i)
-            for j in l:
-                ans.append(list(j))
-        return ans
+        n=len(nums)
+        def recur(index,l):
+            if index==-1:
+                return [l]
+            left=recur(index-1,[nums[index]]+l)
+            right=recur(index-1,l)
+            return left+right
+        
+        return recur(n-1,[])
