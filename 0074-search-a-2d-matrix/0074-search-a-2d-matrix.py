ce55f81811357d2438e1d795301c36549b8565e2
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-04 18:49:35 +0530

    Time: 0 ms (100%), Space: 18.2 MB (59.58%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,24 +1,19 @@
 class Solution:
-    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         n=len(matrix)
         m=len(matrix[0])
-        def applyBS(l,r,index):
-            
-            while l<=r:
-                mid=l+(r-l)//2
-                if matrix[index][mid]==target:
-                    return True
-                elif matrix[index][mid]<target:
-                    l=mid+1
-                else:
-                    r=mid-1
-            return False
-        for i in range(n):
-            l=0
-            r=m-1
-            if target<=matrix[i][r] and target>=matrix[i][l]:
-                if applyBS(l,r,i):
-                    return True
+        for arr in matrix:
+            if arr[0]<=target<=arr[m-1]:
+                l=0
+                r=m-1
+                while l<=r:
+                    mid=l+(r-l)//2
+                    if arr[mid]==target:
+                        return True
+                    elif arr[mid]<target:
+                        l=mid+1
+                    else:
+                        r=mid-1
         return False
+
         
