535e1380ee90c1936bb755dde27e30ad5248f739
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-06 01:15:21 +0530

    Time: 23 ms (14.83%), Space: 17.8 MB (52.67%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,36 +1,19 @@
 class Solution:
-    def combinationSum(self, arr: List[int], t: int) -> List[List[int]]:
-        l=[]
-        p=[]
-        dic={}
-        def ram(arr,t,n,l):
-            if (n,t) in dic:
-                return  
+    def combinationSum(self, arr: List[int], target: int) -> List[List[int]]:
+        n=len(arr)
+        ans=[]
+        arr.sort()
+        def func(i,s,t):
             
             if t==0:
-                p.append(l)
-                return None
-            pick=None
-            notpick=None
-            if (n,t) in dic:
-                return dic[(n,t)]
-            if t-arr[n]>=0:
-                pick=ram(arr,t-arr[n],n,l+[arr[n]])
-            if n!=0:
-                nonpick=ram(arr,t,n-1,l)
-                
-            if pick and notpick:
-                dic[(n,t)]=[pick,notpick]
-            elif pick:
-                dic[(n,t)]=pick
-            elif notpick:
-                dic[(n,t)]=notpick
-            else:
-                return None
+                ans.append(s)
+                return 
+            if t<0:
+                return 
+            if i>n-1:
+                return 
             
-            
-            return 
-        n=len(arr)-1
-        x=ram(arr,t,n,l)
-        return p
-        
+            func(i,s+[arr[i]],t-arr[i])
+            func(i+1,s,t)
+        func(0,[],target)
+        return ans
