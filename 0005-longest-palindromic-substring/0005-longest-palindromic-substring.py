74f1fe8093177282ba36cf900d3405fe3eda5534
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-05 12:53:43 +0530

    Time: 3227 ms (19.4%), Space: 110.4 MB (5.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        #because we are going through all of the substrings
-        
-
-        dp=[[-1]*len(s) for i in range(len(s))]
-        def solve(i,j):
-            if dp[i][j]==1 or dp[i][j]==0:
-                return dp[i][j]
+        n=len(s)
+        dp=[[None]*n for i in range(n)]
+        def checkPalin(i,j):
             if i>=j:
                 dp[i][j]=True
                 return dp[i][j]
-            if s[i]==s[j]:
-                dp[i][j]=solve(i+1,j-1)
+            if dp[i][j]!=None:
                 return dp[i][j]
-            else:
+            if s[i]!=s[j]:
                 dp[i][j]=False
                 return dp[i][j]
-        startpoint=-1
-        leng=0
-        for i in range(0,len(s)):
-            for j in range(i,len(s)):
-                if solve(i,j):
-                    
-                    if leng<j-i+1:
-                        leng=j-i+1
-                        startpoint=i
-        return s[startpoint:leng+startpoint]
+            dp[i][j]=checkPalin(i+1,j-1)   
+            return dp[i][j]
+        maxi=""
+        for i in range(n):
+            for j in range(i,n):
+                if j-i+1>len(maxi):
+
+                    if checkPalin(i,j):
+                        maxi=s[i:j+1]
+        return maxi
+        
