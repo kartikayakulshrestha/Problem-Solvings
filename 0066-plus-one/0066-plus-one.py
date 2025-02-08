a5a909816a6c0feca8e52216f21e022548531635
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-08 16:10:52 +0530

    Time: 0 ms (100%), Space: 17.9 MB (27.13%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,9 +1,21 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         n=len(digits)
-        x=0
-        for i in range(n):
-            x=x*10+digits[i]
-        
-        return list(map(int,str(x+1)))
-        
+        carry=0
+        for i in range(n-1,-1,-1):
+            if i==n-1:
+                if digits[i]==9:
+                    digits[i]=0
+                    carry=1
+                else:
+                    digits[i]+=1
+                    return digits
+            else:
+                if digits[i]==9 and carry:
+                    digits[i]=0
+                else:
+                    digits[i]+=carry
+                    carry=0
+        if carry==1:
+            digits=[1]+digits
+        return digits
