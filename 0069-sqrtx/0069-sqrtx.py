b756f416afddcc19bece48dd1ceed645b63aeb5d
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-08 15:40:12 +0530

    Time: 3 ms (61.43%), Space: 17.9 MB (28.52%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,18 +1,14 @@
 class Solution:
-    def mySqrt(self, n: int) -> int:
+    def mySqrt(self, x: int) -> int:
         l=0
-        r=n
-        mid=l+(r-l)//2
-        ans=-1
+        r=x
+        ans=float("-inf")
         while l<=r:
             mid=l+(r-l)//2
-            if mid*mid ==n:
-                return mid
-            elif mid*mid>n:
-                r=mid-1
-
-            else:
-                ans=max(ans,mid)
+            if mid*mid<=x:
+                ans=max(mid,ans)
                 l=mid+1
+            else:
+                r=mid-1
         return ans
-                
+        
