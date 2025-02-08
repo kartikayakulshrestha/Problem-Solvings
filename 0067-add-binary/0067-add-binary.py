9506edd6efdb41966d2dfb8f850247c2862d5e4c
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-08 16:54:13 +0530

    Time: 0 ms (100%), Space: 17.8 MB (46.68%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,47 +1,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(a)<len(b):
-            b,a=a,b
-        n=len(a)
-        m=len(b)
-        i=n-1
-        j=m-1
-        carry=0
-        ans=""
-        for k in range(min(n,m)):
-            if a[i]=="1" and b[j]=="1":
-                if carry:
-                    ans+="1"
-                else:
-                    ans+="0"
-                    carry=1
-            elif a[i]=="1" or b[j]=="1":
-                if carry:
-                    ans+="0"
-                else:
-                    ans+="1"
-            else:
-                if carry:
-                    ans+="1"
-                    carry=0
-                else:
-                    ans+="0"
-            i-=1
-            j-=1
+        ass=int(a,2)
+        bss=int(b,2)
+
+        tot=ass+bss
         
-        while i>=0 and carry:
-            if a[i]=="1" and carry:
-                
-                ans+="0"
-                carry=1
-                
-            elif a[i]=="0" and carry:
-                ans+="1"
-                carry=0
-            i-=1
-        while i>=0:
-            ans+=a[i]
-            i-=1
-        if carry:
-            ans+="1"   
-        return ans[::-1]
+        return bin(tot)[2:]
