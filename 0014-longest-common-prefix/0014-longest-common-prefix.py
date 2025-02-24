77b9c9f3cc1774e57c9b3cfd85f08ac0fe88a7c6
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-24 17:32:18 +0530

    Time: 4 ms (11.54%), Space: 17.8 MB (78.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,11 +1,17 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        strs.sort(key=len)
-        x=strs[0]
-        n=len(strs)
-        for i in range(0,n):
-            for j in range(min(len(x),len(strs[i]))):
-                if strs[i][j]!=x[j]:
-                    x=strs[i][:j]
-                    break
+        if not strs:
+            return ""
+        
+        x = strs[0]  # Initialize prefix with the first string
+        
+        for i in range(1, len(strs)):  # Start from the second string
+            j = 0
+            while j < len(x) and j < len(strs[i]) and strs[i][j] == x[j]:
+                j += 1
+            x = x[:j]  # Update prefix to matched portion
+            
+            if not x:  # If prefix becomes empty, return immediately
+                return ""
+        
         return x
