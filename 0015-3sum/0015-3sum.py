82a7333edee4feb55a2116d208b969d47b91aaa0
Kartikaya Kulshrestha <126651238+kartikayakulshrestha@users.noreply.github.com>
2025-02-28 15:35:36 +0530

    Time: 1759 ms (7.64%), Space: 22 MB (6.88%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans=[]
-    
         n=len(nums)
-        done=set()
-        for i in range(n):
-            
+        if n>=3 and nums[0]==0 and nums[-1]==0:
+            return [[0,0,0]]
+        visited=set()
+        ans=[]
+        for i in range(n-2):
             l=i+1
             r=n-1
             while l<r:
-                    
-                p=nums[l]+nums[r]+nums[i]
-                v=(nums[i],nums[l],nums[r])
-                    
+                x=(nums[i],nums[l],nums[r])
+                q=[nums[i],nums[l],nums[r]]
+                p=nums[i]+nums[l]+nums[r]
                 if p==0:
-                    if v not in done:
-                        ans.append([nums[i],nums[l],nums[r]])
-                    done.add(v)
+                    if x not in visited:
+                        visited.add(x)
+                        ans.append(q)
                     l+=1
                     r-=1
                 elif p<0:
                     l+=1
-                else:
+                elif p>0:
                     r-=1
         return ans
-                
